db1b6678464acab5fc603b2f7e0df79e98a4f076
Vharsha Thirumeninathan <118044839+Vharsha22122057@users.noreply.github.com>
2023-05-08 13:40:14 +0530

    Delete LAB 7 directory

--- LAB 7/tempCodeRunnerFile.java
+++
@@ -1,4 +0,0 @@
-int secondColumn = Integer.parseInt(data[1]);
-                // int thirdColumn = Integer.parseInt(data[2]);
-                // int fourthColumn = Integer.parseInt(data[3]);
-                // int fifthColumn = Integer.parseInt(data[4]);
